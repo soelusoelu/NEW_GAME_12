7d50339db881160615993150ca1d54dee6aceb09
丹下輝巳 <teru.0701.tt@gmail.com>
2020-01-09 13:51:09 +0900

    エネミーのコンポーネントにenum追加

--- DirectX/Component/EnemyComponent.cpp
+++ DirectX/Component/EnemyComponent.cpp
@@ -8,9 +8,12 @@
 #include "../Component/ComponentManager.h"
 #include "../Component/Collider.h"
 #include "../Component/CircleCollisionComponent.h"
+#include "../Actor/Enemy.h"
 
-EnemyComponent::EnemyComponent(Actor * owner):
+EnemyComponent::EnemyComponent(Actor * owner) :
 	Component(owner),
+	mFlag(SkippedFlag::FALSE),
+	mDestination(Vector2(0.f,0.f)),
 	mCol(nullptr)
 {
 }
@@ -22,12 +25,20 @@ EnemyComponent::~EnemyComponent()
 void EnemyComponent::start()
 {
 	mCol = mOwner->getComponentManager()->getComponent<Collider>();//CircleCollisionComponent
+	mDestination = mOwner->getTransform()->getPosition();
 }
 
 void EnemyComponent::update()
 {
 	//move(search());
 	hit();
+
+	switch ()
+	{
+
+	default:
+		break;
+	}
 }
 
 void EnemyComponent::move(const Vector2 & movement)//移動
@@ -36,6 +47,8 @@ void EnemyComponent::move(const Vector2 & movement)//移動
 	difference.normalize();
 	
 	mOwner->getTransform()->translate(difference * 60.f * Time::deltaTime);//移動処理(60倍する)
+
+	
 }
 
 void EnemyComponent::hit()//当たり判定
@@ -45,18 +58,39 @@ void EnemyComponent::hit()//当たり判定
 	{
 		if (i->getOwner()->getTag() == "Player")//プレイヤーとあたったら(ほんとは穴に落ちる)
 		{
-			Vector2 mVec = Actor::getActorManager()->getPlayer()->getTransform()->getPosition() - mOwner->getTransform()->getPosition();
-			mVec *= -1;
-			mVec.normalize();//エネミーから見たプレイヤーと反対のベクトル
-			mOwner->getTransform()->translate(mVec * 750.f *60.f * Time::deltaTime);//移動処理
+			//プレイヤーのスピードに応じてダメージ
+
+			//フラグをtrueに
+			mFlag = SkippedFlag::TRUE;
+
+
+			//Vector2 mVec = Actor::getActorManager()->getPlayer()->getTransform()->getPosition() - mOwner->getTransform()->getPosition();
+			//mVec *= -1;
+			//mVec.normalize();//エネミーから見たプレイヤーと反対のベクトル
+			//mOwner->getTransform()->translate(mVec * 750.f *60.f * Time::deltaTime);//移動処理
 		}
 
-		if (i->getOwner()->getTag() == "Hole")
+		if (i->getOwner()->getTag() == "Hole")//穴に落ちると即死
 		{
 			Actor::destroy(mOwner);//自分が消える
 		}
 
 	}
+}
+
+Vector2 EnemyComponent::normal()
+{
+	return Vector2();
+}
+
+Vector2 EnemyComponent::shot()
+{
+	return Vector2();
+}
+
+Vector2 EnemyComponent::tackle(const Vector2 & movement)
+{
+	return Vector2();
 }
 
 Vector2 EnemyComponent::search()//プレイヤーの位置情報を受け取りその値を返す
