6d80665efab02e783dd9c7b2129e758c6ab5f1f1
takano <llmn.0419@gmail.com>
2020-01-22 14:23:10 +0900

    ミスってた

--- DirectX/Scene/GamePlay.cpp
+++ DirectX/Scene/GamePlay.cpp
@@ -65,9 +65,9 @@ void GamePlay::updateScene() {
         if (!mActorManager->getPlayer()) { //ゲームオーバー
             nextScene(std::make_shared<Title>());
         }
-        if (!mActorManager->getActor<EnemyActor>()) { //クリア
-            nextScene(std::make_shared<Title>());
-        }
+        //if (!mActorManager->getActor<EnemyActor>()) { //クリア
+        //    nextScene(std::make_shared<Title>());
+        //}
     } else if (mState == GameState::PAUSED) {
 
     }
